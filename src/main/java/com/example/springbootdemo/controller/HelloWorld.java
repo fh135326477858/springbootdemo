27f4bb7703fca300d103d114cfd3ec53c20937d4
1033689956@qq.com <a13526477858>
2019-03-19 21:27:45 +0800

    提交

---
+++ src/main/java/com/example/springbootdemo/controller/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloWorld {
+
+    @ResponseBody
+    @RequestMapping("/index")
+    public String hello(ModelAndView model){
+        model.addObject("hello world");
+        return "Hello World!";
+    }
+
+    @RequestMapping("/thymeleaf")
+    public String hello2(Map<String,Object> map){
+        map.put("test","test");
+        map.put("user",Arrays.asList("111", "222"));
+        return "ok";
+    }
+
+    @RequestMapping("/thymeleaf2")
+    public String hello3(){
+        return "ok";
+    }
+
+}
