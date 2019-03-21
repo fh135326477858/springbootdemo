9a49b2f928817b19096f8f42dbff91e256e2719a
1033689956@qq.com <a13526477858>
2019-03-22 00:15:44 +0800

    静态资源导入与国际化

---
+++ src/main/java/com/example/springbootdemo/config/SpringMvcConfig.java
@@ -0,0 +1,31 @@
+package com.example.springbootdemo.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/login.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
+}
+
